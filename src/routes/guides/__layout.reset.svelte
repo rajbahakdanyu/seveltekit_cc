fda6528f3a740306115039cbba17a99fe0c772f8
danyu rajbahak <rajbahakdanyu@gmail.com>
2022-01-17 19:35:17 +0545

    Added a layout file

---
+++ src/routes/guides/__layout.reset.svelte
@@ -0,0 +1,49 @@
+<script>
+    import Title from "$lib/title.svelte"
+    import "../../styles/global.css"
+</script>
+
+<header>
+    <nav>
+        <Title title="Danyu Gaming Guides" />
+        <div class="links">
+            <a href="/">Home</a>
+            <a href="/about">About</a>
+            <a href="/guides">Guides</a>
+        </div>
+    </nav>
+</header>
+
+<main>
+    <slot />
+</main>
+
+<footer>
+    <p>Copyright 2022 Danyu Gaming Guides</p>
+</footer>
+
+<style>
+    header {
+        text-align: center;
+        background: rgba(0, 0, 0, 0.1);
+    }
+    main {
+        max-width: 960px;
+        margin: 20px auto;
+    }
+    footer {
+        text-align: center;
+    }
+    nav {
+        display: flex;
+        align-items: center;
+        max-width: 960px;
+        margin: 0 auto;
+    }
+    .links {
+        margin-left: auto;
+    }
+    a {
+        margin-left: 10px;
+    }
+</style>
