80feaf81bfac7af227d03e17fc3f8bed347518e3
danyu rajbahak <rajbahakdanyu@gmail.com>
2022-01-17 19:31:01 +0545

    Updated guides/index

--- src/routes/guides/index.svelte
+++ src/routes/guides/index.svelte
@@ -1,7 +1,35 @@
+<script context="module">
+    export async function load({ fetch }) {
+        const res = await fetch("/guides.json")
+        const { guides } = await res.json()
+        console.log(guides)
+        if (res.ok) {
+            return {
+                props: {
+                    guides,
+                },
+            }
+        }
+        return {
+            status: res.status,
+            error: new Error("Could not fetch the guides"),
+        }
+    }
+</script>
+
+<script>
+    export let guides
+</script>
+
 <div class="guides">
     <ul>
-        <li><a href="/">Guide 1 </a></li>
-        <li><a href="/">Guide 2 </a></li>
+        {#each guides as guide}
+            <li>
+                <a sveltekit:prefetch href={`/guides/${guide.id}`}
+                    >{guide.title}</a
+                >
+            </li>
+        {/each}
     </ul>
 </div>
 
@@ -9,12 +37,10 @@
     .guides {
         margin-top: 20px;
     }
-
     ul {
         list-style-type: none;
         padding: 0;
     }
-
     a {
         display: inline-block;
         margin-top: 10px;
