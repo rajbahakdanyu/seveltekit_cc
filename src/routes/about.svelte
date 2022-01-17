5a8a67eb03955da1173bac86f2c6661d920be91f
danyu rajbahak <rajbahakdanyu@gmail.com>
2022-01-17 19:26:17 +0545

    Updated about page

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -1,12 +1,19 @@
+<script>
+</script>
+
 <div class="about">
     <h2>About Us</h2>
-
     <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique
-        mollitia, itaque ratione perferendis culpa a pariatur incidunt, ea autem
-        minima veritatis blanditiis porro reprehenderit dolores? Accusantium
-        numquam quaerat earum minima.
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sunt laborum
+        magnam deleniti recusandae, quis aperiam, architecto neque, voluptatem
+        dicta expedita aut id voluptate omnis consectetur dolore! Quo quae
+        perferendis vel dignissimos quasi illum aperiam dolores, id, laudantium
+        voluptatibus fugiat accusantium impedit velit modi ratione iure qui
+        culpa! Repellendus cupiditate aliquid quam magni. Laudantium, laboriosam
+        quibusdam. Provident, quae incidunt! Aspernatur, vel?
     </p>
+    <a href="/guides">View Guides</a>
+    <a href="/">Home</a>
 </div>
 
 <style>
