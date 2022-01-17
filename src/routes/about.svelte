e9332d8cbb4ad5893b05a624d280dac97b26cb9d
danyu rajbahak <rajbahakdanyu@gmail.com>
2022-01-17 18:49:08 +0545

    Added an about page

---
+++ src/routes/about.svelte
@@ -0,0 +1,18 @@
+<div class="about">
+    <h2>About Us</h2>
+
+    <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique
+        mollitia, itaque ratione perferendis culpa a pariatur incidunt, ea autem
+        minima veritatis blanditiis porro reprehenderit dolores? Accusantium
+        numquam quaerat earum minima.
+    </p>
+</div>
+
+<style>
+    .about {
+        text-align: center;
+        display: block;
+        margin: 20px auto;
+    }
+</style>
