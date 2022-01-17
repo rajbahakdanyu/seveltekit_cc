bd0e3def5af56b7f5503b39b6e5df65d5307f3b3
danyu rajbahak <rajbahakdanyu@gmail.com>
2022-01-17 19:29:46 +0545

    Added a layout components

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,30 @@
+<script>
+    import Title from "$lib/title.svelte"
+    import "../styles/global.css"
+</script>
+
+<header>
+    <Title title="Danyu Gaming Guides" />
+</header>
+
+<main>
+    <slot />
+</main>
+
+<footer>
+    <p>Copyright 2022 Chaina Guys Enjoy</p>
+</footer>
+
+<style>
+    header {
+        display: flex;
+        justify-content: center;
+    }
+    main {
+        max-width: 960px;
+        margin: 20px auto;
+    }
+    footer {
+        text-align: center;
+    }
+</style>
